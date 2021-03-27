ca5aebdf42baa21a39c3d8a06ce96a5c09c5c175
bhohnco <bryanhohn13@gmail.com>
2021-03-27 09:58:42 -0600

    Testing push

--- sript.js
+++ sript.js
@@ -2,3 +2,36 @@
 3: Class methods use array and object prototypes - for loops are not used in the application. Application shows strong effort towards organization, content, and refactoring. There are multiple scenarios of destructuring to help enhance readability. Application is effectively broken into logical components, but violate the principle of SRP.
 2: Class methods use a mix of array and object prototypes and for loops. Application runs but the code has long methods, unnecessary or poorly named variables, and needs significant refactoring. There is no usage of destructuring. Divisions of logic into classes are inconsistent or unclear.
 1: Application generates syntax error or crashes during execution. Application logic shows poor decomposition with too much logic mashed together.
+
+    Test-Driven Development
+4: Application covers all aspects of the application including various flows and covers both happy/sad paths.
+3: Application is well tested but fails to cover some features and only tests for happy paths. Tests use smaller, sample data files as input rather than the large, original data files.
+2: Application makes some use of tests, but the coverage is insufficient given project requirements.
+1: Application does not demonstrate strong use of TDD.
+    User Interface
+4: The application can stand on its own to be used by an instructor without guidance from a developer on the team. The application is fully responsive and the UI does not detract from the UX. In addition, the application has clearly had special consideration around usability on devices.
+3: The application has many strong displays/interactions, but a few holes in lesser-used displays. The application is fully responsive and the UI does not detract from the UX.
+2: The application shows effort in the interface, but the result is not effective. The evaluator has some difficulty using the application when reviewing the features in the users’ needs. Application is not responsive on other devices.
+1: The application is confusing or difficult to use.
+
+    Test-Driven Development
+4: Application covers all aspects of the application including various flows and covers both happy/sad paths.
+3: Application is well tested but fails to cover some features and only tests for happy paths. Tests use smaller, sample data files as input rather than the large, original data files.
+2: Application makes some use of tests, but the coverage is insufficient given project requirements.
+1: Application does not demonstrate strong use of TDD.
+    User Interface
+4: The application can stand on its own to be used by an instructor without guidance from a developer on the team. The application is fully responsive and the UI does not detract from the UX. In addition, the application has clearly had special consideration around usability on devices.
+3: The application has many strong displays/interactions, but a few holes in lesser-used displays. The application is fully responsive and the UI does not detract from the UX.
+2: The application shows effort in the interface, but the result is not effective. The evaluator has some difficulty using the application when reviewing the features in the users’ needs. Application is not responsive on other devices.
+1: The application is confusing or difficult to use.
+
+    Test-Driven Development
+4: Application covers all aspects of the application including various flows and covers both happy/sad paths.
+3: Application is well tested but fails to cover some features and only tests for happy paths. Tests use smaller, sample data files as input rather than the large, original data files.
+2: Application makes some use of tests, but the coverage is insufficient given project requirements.
+1: Application does not demonstrate strong use of TDD.
+    User Interface
+4: The application can stand on its own to be used by an instructor without guidance from a developer on the team. The application is fully responsive and the UI does not detract from the UX. In addition, the application has clearly had special consideration around usability on devices.
+3: The application has many strong displays/interactions, but a few holes in lesser-used displays. The application is fully responsive and the UI does not detract from the UX.
+2: The application shows effort in the interface, but the result is not effective. The evaluator has some difficulty using the application when reviewing the features in the users’ needs. Application is not responsive on other devices.
+1: The application is confusing or difficult to use.
